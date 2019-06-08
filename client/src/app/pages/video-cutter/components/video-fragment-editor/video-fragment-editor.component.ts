5d4ee9faccc307f03e019e9f394e61bc1f07760d
alex-w-dev <yampi.ru@gmail.com>
2019-06-08 20:13:45 +0500

    fix bug: not ault play fragment on video fragment change

--- client/src/app/pages/video-cutter/components/video-fragment-editor/video-fragment-editor.component.ts
+++ client/src/app/pages/video-cutter/components/video-fragment-editor/video-fragment-editor.component.ts
@@ -9,21 +9,32 @@ import Helper from "../../../../shared/classes/helper";
   styleUrls: ['./video-fragment-editor.component.scss']
 })
 export class VideoFragmentEditorComponent implements OnInit {
-  @Input() videoFragment: IVideoFragmentModel;
+  @Input() set videoFragment(value: IVideoFragmentModel) {
+    this._videoFragment = value;
+    this.lastVideoFragmentSettedTime = Date.now();
+  }
+  get videoFragment(): IVideoFragmentModel {
+    return this._videoFragment;
+  }
+  private _videoFragment: IVideoFragmentModel;
   @Input() video: HTMLVideoElement;
   @Output() onFragmentChange: EventEmitter<IVideoFragmentModel> = new EventEmitter();
+
+  private lastVideoFragmentSettedTime: number = 0;
 
   constructor(
     private videoCutterService: VideoCutterService,
   ) { }
 
   ngOnInit() {
-    if (!this.videoFragment) throw new Error('Input videoFragment is required');
+    if (!this._videoFragment) throw new Error('Input videoFragment is required');
     if (!this.video) throw new Error('Input video is required');
   }
 
   onTimeChange(time) {
+    if (this.lastVideoFragmentSettedTime + 1000 > Date.now()) return;
+
     this.videoCutterService.startPlaySelectedVideoRecursively(time, time + 1);
-    this.onFragmentChange.emit(this.videoFragment);
+    this.onFragmentChange.emit(this._videoFragment);
   }
 }
